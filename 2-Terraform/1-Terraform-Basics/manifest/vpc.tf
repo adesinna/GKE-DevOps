858ae338d88cc74dbbf033003862ee5f06b26e92
adesinna <aladesaea@yahoo.com>
2025-08-28 14:42:07 -0400

    vpc

---
+++ 2-Terraform/1-Terraform-Basics/manifest/vpc.tf
@@ -0,0 +1,16 @@
+provider "google" {
+  project     = "terraform-470417"
+  region      = "us-central1"
+}
+
+resource "google_compute_subnetwork" "polom-subnet" {
+  name          = "test-subnet"
+  ip_cidr_range = "10.2.0.0/16"
+  region        = "us-central1"
+  network       = google_compute_network.polom-vpc.id
+}
+
+resource "google_compute_network" "polom-vpc" {
+  name                    = "polom-vpc"
+  auto_create_subnetworks = false
+}
