40a640b46fdbccf8c7b57cd19c3639684e463ebb
adesinna <aladesaea@yahoo.com>
2025-08-28 15:12:36 -0400

    sa

--- 2-Terraform/1-Terraform-Basics/manifest/vpc.tf
+++ 2-Terraform/1-Terraform-Basics/manifest/vpc.tf
@@ -1,6 +1,18 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "7.0.1"
+    }
+  }
+}
+
+
 provider "google" {
   project     = "terraform-470417"
   region      = "us-central1"
+  zone        = "us-central1-a"
+  credentials = "/home/ubuntu/terraform-sa-key"
 }
 
 resource "google_compute_subnetwork" "polom-subnet" {
