d8ae2f97c7bc85902d544005d85a329e472c0068
adesinna <aladesaea@yahoo.com>
2025-09-01 08:30:22 -0400

    update to just one zone

--- 5-Gateway-API/Private-Standard-Cluster-GWA/8-node-pool.tf
+++ 5-Gateway-API/Private-Standard-Cluster-GWA/8-node-pool.tf
@@ -1,14 +1,14 @@
 # Resource: GKE Linux Node Pool
 resource "google_container_node_pool" "linux_nodepool_1" {
   name       = "${local.name}-linux-nodepool-1"
-  location   = var.region
+  location   = var.region  #"us-central1-a"  match cluster zone
   cluster    = google_container_cluster.gke_cluster.name
   initial_node_count = 1 # the number of nodes to create in each zone
   node_locations = ["us-central1-a"] # 1 zone
   autoscaling {
     min_node_count = 1
     max_node_count = 3
-    location_policy = "ANY"
+//    location_policy = "ANY"
   }
   node_config {
     preemptible  = true
