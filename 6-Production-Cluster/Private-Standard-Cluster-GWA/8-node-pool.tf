8440e794786a7058fbaa17c794ea46660dcb6235
adesinna <aladesaea@yahoo.com>
2025-09-01 19:55:26 -0400

    nodes

--- 6-Production-Cluster/Private-Standard-Cluster-GWA/8-node-pool.tf
+++ 6-Production-Cluster/Private-Standard-Cluster-GWA/8-node-pool.tf
@@ -4,11 +4,11 @@ resource "google_container_node_pool" "linux_nodepool_1" {
   location   = var.region  #"us-central1-a"  match cluster zone
   cluster    = google_container_cluster.gke_cluster.name
   initial_node_count = 1 # the number of nodes to create in each zone
-  node_locations = ["us-central1-a"] # 1 zone
+//  node_locations = ["us-central1-a"] # 1 zone
   autoscaling {
     min_node_count = 1
     max_node_count = 3
-//    location_policy = "ANY"
+    location_policy = "ANY"
   }
   node_config {
     preemptible  = true
